f75318d0f0f2e87f6b8cc6fb8dc0563a7f9944ba
acomjean <acomjean@gmail.com>
2021-03-21 00:33:51 -0400

    more updates to game skeleton

--- p1/README.md
+++ p1/README.md
@@ -6,6 +6,8 @@ Aram Comjean
 
 ## Outside resources
 na
-
+https://vuejs.org/v2/style-guide/#Private-property-names-essential
+ https://en.wikipedia.org/wiki/Tit_for_tat
+https://stackoverflow.com/questions/45978055/how-to-get-the-value-of-button-when-clicked-using-vue-js
 ## Notes for instructor
 na
--- p1/script.js
+++ p1/script.js
@@ -2,20 +2,85 @@
 const Game = {
     data() {
         return {
-            rounds: [
+            name: "",
+            gameHistory: [
                 { number: 1, winner: 'Player', coin: 'heads', choice: 'heads' },
                 { number: 2, winner: 'Computer', coin: 'tails', choice: 'heads' },
                 { number: 3, winner: 'Computer', coin: 'heads', choice: 'tails' },
                 { number: 4, winner: 'Player', coin: 'tails', choice: 'tails' },
-            ]
+            ],
+            gameState: "beginning",
+            gameNumber: 0,
+            scoreUser: 0,
+            scoreComputer: 0
         }
     },
+
+    computed: {
+
+        // get id of computer algorithm
+        oponentID() {
+            return 1;
+        }
+    },
+
+
     methods: {
+        startGame() {
+            this.guess = 1;
+            this.gameRounds = [];
+            this.gameState = "started";
+
+        },
+
         deleteRound(number) {
             this.rounds = this.rounds.filter(round => round.number != number);
+        },
+
+        takeTurn(userMove) {
+
+            let scoreUser = this.scoreUser;
+            let scoreComputer = this.scoreComputer;
+
+            computerMove = "C";
+            this.gameNumber += 1;
+            switch (userMove) {
+                case 'C':
+                    if (computerMove == "C") {
+                        scoreUser += 3;
+                        scoreComputer += 3;
+                    } else {
+                        scoreUser += 1;
+                        scoreComputer += 4;
+                    }
+                    break;
+
+                case 'D':
+                    if (computerMove == "C") {
+                        scoreUser += 4;
+                        scoreComputer += 1;
+                    } else {
+                        scoreUser += 2;
+                        scoreComputer += 2;
+                    }
+                    break;
+
+                default:
+                    console.log('err')
+            }
+
+            console.log('compuiter:' + scoreComputer + " User score " + scoreUser)
+            this.scoreComputer = scoreComputer;
+            this.scoreUser = scoreUser;
         }
+
+
     }
+
+
 }
+
+
 
 const RoundDetail = {
     name: 'RoundDetail',
