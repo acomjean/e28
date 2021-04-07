bba16fd9769bd5b6f5f1324ca7742a7905e183b1
aram <acomjean@gmail.com>
2021-04-06 21:32:59 -0400

    week9 intial

---
+++ zipfoods/src/components/pages/HomePage.vue
@@ -0,0 +1,21 @@
+<template>
+    <div id="home-page">
+        <p>
+            ZipFoods !
+        </p>
+
+        <show-featured v-bind:category="'snacks'"></show-featured>
+    </div>
+</template>
+
+<script>
+import ShowFeatured from "@/components/ShowFeatured.vue";
+export default {
+    components: {
+        "show-featured": ShowFeatured,
+    },
+};
+</script>
+
+<style>
+</style>
---
+++ zipfoods/src/components/pages/ProductPage.vue
@@ -0,0 +1,18 @@
+<template>
+    <div id="product-page">
+        <h1>Product: {{ id }}</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        id: {
+            type: String,
+        },
+    },
+};
+</script>
+
+<style>
+</style>
