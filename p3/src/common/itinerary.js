9d7d4f446beb5d8ae0e3731adbe7279055e283b3
aram <acomjean@gmail.com>
2021-05-08 21:42:31 -0400

    add vuex step1

--- p3/src/common/itinerary.js
+++ p3/src/common/itinerary.js
@@ -1,47 +1,83 @@
-import { createItinerary, createLogger } from 'vuex'
+import { createStore, createLogger } from 'vuex'
 import { axios } from "@/common/app.js";
+
 
 // Only load the createLogger plugin when in development mode
 const debug = process.env.NODE_ENV !== 'production';
 const plugins = debug ? [createLogger({})] : [];
 
-export const itinerary = createItinerary({
+export const store = createStore({
+    plugins,
+    state() {
+        return {
+            user: 5,
+            cartCount: 5,
+            itineraryArray: [],
+        }
+    },
+
+    mutations: {
+        setItinerary(state, payload) {
+            console.log("setProductssetItinerary");
+            state.itineraryArray = payload;
+        },
+    },
+
+    actions: {
+        fetchItinerary(context) {
+            axios.get("itinerary/query", {
+                params: {
+                    user_id: this.user,
+                },
+            }).then((response) => {
+                console.log("+-fetchItinerary    -------------+  ");
+                console.log(response.data.itinerary);
+                console.log("+- -------------    -------------+  ");
+                context.commit('setItinerary', response.data.itinerary);
+            });
+        },
+    },
+
+})
+
+
+
+
+
+/*
+import { createStore, createLogger } from 'vuex'
+import { axios } from "@/common/app.js";
+
+// Only load the createstore plugin when in development mode
+const debug = process.env.NODE_ENV !== 'production';
+const plugins = debug ? [createLogger({})] : [];
+
+export const itineraryvuex = createStore({
     plugins,
     state() {
         return {
             itinerary: [],
-            user: null,
         }
     },
-    // Methods used to alter the state of our store
+
     mutations: {
-        setCartCount(state, payload) {
-            state.cartCount = payload;
-        },
-        setProducts(state, payload) {
-            state.products = payload;
-        },
-        setUser(state, payload) {
-            state.user = payload;
+        setItinerary(state, payload) {
+            state.itinerary = payload;
         }
+
     },
     // Methods that can contain async. code
     // Can not directly alter the state - has to change state
     // by committing mutations
     actions: {
-        fetchItinerary(context) {
+        fetchItinerary() {
             axios.get("itinerary/query", {
                 params: {
                     user_id: this.userID,
                 },
-            })
-        },
-        authUser(context) {
-            axios.post('auth').then((response) => {
-                if (response.data.authenticated) {
-                    context.commit('setUser', response.data.user);
-                }
-            });
+            })//.then((response) => {
+            //   context.commit('setItinerary', response.data);
+            // });
         },
     },
     getters: {
@@ -52,3 +88,4 @@ export const itinerary = createItinerary({
         }
     }
 })
+*/
--- p3/src/main.js
+++ p3/src/main.js
@@ -1,11 +1,17 @@
 import { createApp } from 'vue'
 import { createRouter, createWebHistory } from 'vue-router';
 
-import App from './App.vue'
+
 import HomePage from '@/components/pages/HomePage.vue';
 import ArtistList from '@/components/pages/ArtistListPage.vue';
 import Itinerary from '@/components/pages/ItineraryPage.vue';
+import ItineraryTest from '@/components/pages/ItineraryPage2.vue';
 
+
+//import { store as itinerary } from '@/common/itinerary.js'
+import { store } from '@/common/itinerary.js'
+
+import App from './App.vue'
 
 const router = createRouter({
     history: createWebHistory(),
@@ -13,7 +19,10 @@ const router = createRouter({
         { path: '/', component: HomePage },
         { path: '/browse-artists', component: ArtistList, props: true },
         { path: '/itinerary', component: Itinerary, props: true },
+        { path: '/itinerary_test', component: ItineraryTest, props: false },
+
+
     ]
 });
 
-createApp(App).use(router).mount('#app');
+createApp(App).use(router).use(store).mount('#app');
