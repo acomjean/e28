91f625a60eda669a3a3c73cc2755a879162a452e
aram <acomjean@gmail.com>
2021-05-09 22:46:01 -0400

    User Persistance...

--- p3/src/components/pages/LoginPage.vue
+++ p3/src/components/pages/LoginPage.vue
@@ -85,7 +85,7 @@ export default {
         logout() {
             axios.post("logout").then((response) => {
                 if (response.data.success) {
-                    this.$store.commit("setUser", null);
+                    this.$store.commit("setUser", false);
                 }
             });
         },
